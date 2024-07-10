53492343634363a0c298340f37a60a969a08e6d3
Rahul <rahulyadav1209ts@gmail.com>
2024-07-10 13:10:56 +0530

    css fix

--- components/academy/index.jsx
+++ components/academy/index.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import Essentials from "../essentials";
 import Alphahub from "../alphaHub";
 import { useSearchParams } from "next/navigation";
@@ -9,21 +9,34 @@ import DegenGlossary from "../degenGlossary";
 import Tabs from "../UI/tabs";
 import { narrativeData } from "@/utils/data";
 
-function Academy() {
+function AcademyContent() {
   const searchParam = useSearchParams();
   const newParam = searchParam.get("tab");
+
   return (
-    <div className={styles.mainContainer}>
-      <div className={styles.hero} />
+    <>
       {newParam === "essentials" ? (
         <Essentials />
       ) : (
         <Alphahub data={narrativeData} />
       )}
+    </>
+  );
+}
+
+function Academy() {
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.hero} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <AcademyContent />
+      </Suspense>
       <div className="border" />
       <Creators />
       <div className="border" />
-      <DegenGlossary />
+      <Suspense fallback={<div>Loading Glossary...</div>}>
+        <DegenGlossary />
+      </Suspense>
       <Tabs />
     </div>
   );
